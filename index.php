0423716d95549cccf39dc7d949d3831d3a62b7f0
Gabriel Kristersen <gabrielkristensen@gmail.com>
2016-09-19 14:56:32 +0200

    layout

--- index.php
+++ index.php
@@ -1,4 +1,7 @@
-<?php require_once("db_con.php");  $connection = new mysqli(DB_HOST, DB_USER, DB_PASS, DB_NAME); $connection->set_charset("utf8");  ?> <!doctype html> <html> <head> <meta charset="utf-8"> <title>Chuck Norris Facts</title> <link rel="stylesheet" type="text/css" href="css/styles.css">	 </head>  <body>     	 <header> <h1>Chuck Norris Facts</h1><?php 
+<?php require_once("db_con.php");  $connection = new mysqli(DB_HOST, DB_USER, DB_PASS, DB_NAME); $connection->set_charset("utf8");  ?> <!doctype html> <html> <head> <meta charset="utf-8"> <title>Chuck Norris Facts</title> <link rel="stylesheet" type="text/css" href="css/styles.css">	 </head>  
+<body>     	 
+<header> 
+<h1>Chuck Norris Facts</h1><?php 
 		 
 		 if ($connection->connect_error) {
 			die('Connect Error: ' . $connection->connect_error);
@@ -6,8 +9,8 @@
 			echo '<span class="hint">[Successful connection to MySQL database!]</span>';
 			}
 		 
-		 ?>
-         </header>
+		 ?>	
+</header>
          <?php 
 		 $jokedata = $connection->query("SELECT * FROM joke ORDER BY id DESC"); 
 		 $joke = $data->fetch_assoc();
@@ -23,4 +26,5 @@
             </div>';
 			echo '<!-- single joke end -->';
 		 ?>  
-</body> </html>
+</body> 
+</html>
